69449d76df3d1f121dac708163ec9d47eb860f6a
ciatph <ciat.ph@gmail.com>
2022-06-03 19:08:39 +0800

    fix: Fix typo errors and restore re-ignore xlsx files.

--- src/batch_local.js
+++ src/batch_local.js
@@ -1,10 +1,10 @@
-const parseDayForecast = require('../src/scripts/parse_day')
+const parseDayForecast = require('./scripts/parse_day')
 const { REGIONS } = require('./utils/tendaycsv/constants')
 
 // Parse and extract relevant data from local Excel files to CSV files
 const batchLocal = () => {
-  for (let i = 1; i <= 10; i += 1) {
-    parseDayForecast(`./data/local/day${i}.xls`, REGIONS.BICOL)
+  for (let i = 1; i <= 3; i += 1) {
+    parseDayForecast(`./data/local/day${i}.xlsx`, REGIONS.BICOL)
   }
 }
 
